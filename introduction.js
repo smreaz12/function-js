afe0cf1208a8c9a11aa0677bba5e9188dac3bf63
smreaz12 <smreaz555@gmail.com>
2026-01-31 23:47:29 +0600

    test function

--- introduction.js
+++ introduction.js
@@ -5,3 +5,25 @@ function number(){
 }
 
 number();
+
+
+
+function addNumber(num1,num2){
+    const sum = num1+num2;
+    console.log(sum);
+}
+
+addNumber(22,25);
+addNumber(22,10);
+
+
+//
+function addNumber(num1,num2,num3=0,num4=0){
+    const sum = num1+num2+num3+num4;
+    console.log(sum);
+}
+
+addNumber(22,25);
+addNumber(22,10);
+addNumber(33,8,12);
+addNumber(55,10,65,12);
