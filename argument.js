9e5ec406500b380abb1c4041ef0900935f8b0b46
smreaz12 <smreaz555@gmail.com>
2026-02-01 13:39:16 +0600

    argument in function

---
+++ argument.js
@@ -0,0 +1,22 @@
+function double_number(num1){
+    const double = num1*2;
+    console.log(double);
+}
+
+double_number(55)
+console.log("-------------");
+double_number(25);
+
+const number = 50;
+double_number(number);
+
+double_number(50);
+
+//--arrow function diye kora holo--//
+const multinumber=num=> num*num;
+
+console.log(multinumber(10));
+
+//
+const twoNumber =(num1,num2)=>num1*num2;
+console.log(twoNumber(10,20));
